5a07aa000aecf65aa9211d9b4127a7dfd05a594a
wwwkamalghahreman77-png <www.kamalghahreman77@gmail.com>
2026-07-16 01:25:01 +0330

    Refactor scoring logic in futures_scanner.py

--- futures/futures_scanner.py
+++ futures/futures_scanner.py
@@ -7,31 +7,25 @@ def scan_futures():
 
     results = []
 
-
     for signal in signals:
 
         score = 0
         reasons = []
 
-
         change = signal.get("change", 0)
         volume = signal.get("volume", 0)
-
 
         if abs(change) >= 3:
             score += 30
             reasons.append("حرکت قوی قیمت")
 
-
         if volume >= 10000000:
             score += 40
             reasons.append("حجم بالای معاملات")
 
-
         elif volume >= 1000000:
             score += 20
             reasons.append("افزایش حجم")
-
 
         if score >= 20:
 
@@ -40,6 +34,5 @@ def scan_futures():
             signal["reasons"] = reasons
 
             results.append(signal)
-
 
     return results
