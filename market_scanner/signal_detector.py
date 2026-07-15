ea2dbb6654769c008a8b66214a81e0cd73c6a0f4
wwwkamalghahreman77-png <www.kamalghahreman77@gmail.com>
2026-07-15 22:33:31 +0330

    Refactor signal scoring logic in signal_detector.py

--- market_scanner/signal_detector.py
+++ market_scanner/signal_detector.py
@@ -15,26 +15,57 @@ def scan_for_signals():
         score = 0
         reasons = []
 
-        # رشد قیمت
-        if coin["change"] >= 5:
-            score += 30
-            reasons.append("افزایش قیمت")
+        change = coin.get("change", 0)
+        volume = coin.get("volume", 0)
 
-        # حجم بالا
-        if coin["volume"] >= 10000000:
-            score += 40
-            reasons.append("حجم معاملات بالا")
-        elif coin["volume"] >= 1000000:
+        # حرکت قیمت
+        if change >= 3:
             score += 20
-            reasons.append("افزایش حجم")
+            reasons.append("حرکت قیمتی مثبت")
+
+        # حجم غیرعادی
+        if volume >= 10000000:
+            score += 40
+            reasons.append("ورود حجم سنگین")
+
+        elif volume >= 1000000:
+            score += 25
+            reasons.append("افزایش حجم معاملات")
+
+        # خریدهای سنگین
+        if coin.get("buys", 0) > coin.get("sells", 0):
+            score += 20
+            reasons.append("قدرت خریدار بیشتر است")
+
+        # نقدینگی
+        if coin.get("liquidity", 0) >= 500000:
+            score += 20
+            reasons.append("نقدینگی مناسب")
+
 
         if score >= 50:
+
             signals.append({
+
                 "symbol": coin["symbol"],
+
+                "type": (
+                    "LONG"
+                    if change >= 0
+                    else "SHORT"
+                ),
+
+                "price": coin.get("price", 0),
+
                 "score": score,
+
                 "reasons": reasons,
-                "change": coin["change"],
-                "volume": coin["volume"]
+
+                "change": change,
+
+                "volume": volume
+
             })
+
 
     return signals
